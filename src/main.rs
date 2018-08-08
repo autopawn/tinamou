f34e80524bb8961f084777df661ae26f23049eb0
Autopawn <franciscojacb@gmail.com>
2018-08-07 23:01:49 -0400

    Working example with queries to dbpedia.

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,46 @@ extern crate hyper;
 
 mod hashjoin;
 
+use timely::dataflow::operators::generic::source;
 use std::io::{BufRead, BufReader};
 
 use timely::dataflow::operators::Inspect;
-use timely::dataflow::operators::ToStream;
 use hashjoin::HashJoin; // Own hashjoin operator.
 
 use hyper::Client;
 use hyper::Url;
+use hyper::client::response::Response;
 
-fn map2str(strs : Vec<&str>) -> Vec<String> {
-    // Creates a vector of String from a vector of references to constant strs.
-    let mut st : Vec<String> = Vec::new();
-    for val in strs {
-        st.push(String::from(val));
+fn response_to_table(res: Response) -> Vec<Vec<String>> {
+    // Split request in lines, create a table entry for each line but the first one.
+    let mut table: Vec<Vec<String>> = Vec::new();
+    let mut first = true;
+    for line in BufReader::new(res).lines() {
+        if first {
+            first = false;
+            continue;
+        }
+        let linep = line.unwrap();
+        let mut split = linep[1..linep.len()-1].split("\",\"");
+
+        let mut entry : Vec<String> = Vec::new();
+        for val in split {
+            entry.push(val.to_owned());
+        }
+        table.push(entry);
     }
-    return st;
+    // Return table
+    table
 }
 
 fn main() {
 
     timely::example(|scope| {
 
-        let query1 = "
-        select distinct ?person ?name ?city where {
-          ?person rdf:type foaf:Person .
-          ?person foaf:name ?name .
-          ?city rdf:type schema:City .
-          ?city foaf:name ?name
-        } LIMIT 1000
-        "; // Persons that have the same name than a city.
+        // Create a http client using hyper:
+        let client = Client::new();
 
-        let query2 = "
-        select distinct ?city ?hospital where {
-          ?city rdf:type schema:City .
-          ?hospital rdf:type schema:Hospital .
-          ?hospital dbp:location ?city .
-        } LIMIT 1000
-        "; // Hospitals and their cities.
+        // Create a query preset for both queries:
 
         let mut url = Url::parse("http://dbpedia.org/sparql").unwrap();
         url.query_pairs_mut().append_pair("default-graph-uri","http://dbpedia.org");
@@ -50,69 +52,74 @@ fn main() {
         url.query_pairs_mut().append_pair("timeout","30000");
         url.query_pairs_mut().append_pair("debug","on");
         url.query_pairs_mut().append_pair("run","+Run+Query+");
-        url.query_pairs_mut().append_pair("query",query1);
 
-        // Create a http client using hyper:
-        let client = Client::new();
+        let query1 = "
+        select distinct ?person ?name where {
+            ?person rdf:type foaf:Person .
+            ?person foaf:name ?name .
+            FILTER(REGEX(STR(?name),\"^Ab+\"))
+        } LIMIT 8000
+        "; // Persons and their lastnames, as long as they start with "Ab".
 
-        let res = client.get(url).send().unwrap();
-        assert_eq!(res.status, hyper::Ok);
+        let query2 = "
+        select distinct ?place ?name where {
+            ?place rdf:type schema:Place .
+            ?person foaf:name ?name .
+            FILTER(REGEX(STR(?name),\"^Ab+\"))
+        } LIMIT 8000
+        "; // Places and their names, as long as they start with "Ab".
 
-        for line in BufReader::new(res).lines() {
-            println!("{}", line.unwrap());
-        }
 
+        // Define a source in the timely-dataflow graph for the first query.
+        let stream_1 = source(scope, "tablequery1", |capability| {
+            // Pass capability to an Option container so that it is free when its set to None.
+            let mut capability = Some(capability);
+            // Set request for query 1
+            let mut request = url.clone();
+            request.query_pairs_mut().append_pair("query",query1);
+            // Perform request and get table:
+            let res = client.get(request).send().unwrap();
+            assert_eq!(res.status, hyper::Ok);
+            let table = response_to_table(res);
+            // Transform table into an iterator:
+            let mut iterator = table.into_iter().fuse();
+            // Closure that is called when this source node gets control:
+            move |output| {
+                if let Some(element) = iterator.next() {
+                    let mut session = output.session(capability.as_ref().unwrap());
+                    session.give(element);
+                } else {
+                    capability = None;
+                }
+            }
+        });
 
+        // Define a source in the timely-dataflow graph for the second query.
+        let stream_2 = source(scope, "tablequery2", |capability| {
+            // Pass capability to an Option container so that it is free when its set to None.
+            let mut capability = Some(capability);
+            // Set request of query 2
+            let mut request = url.clone();
+            request.query_pairs_mut().append_pair("query",query2);
+            // Perform request and get table:
+            let res = client.get(request).send().unwrap();
+            assert_eq!(res.status, hyper::Ok);
+            let table = response_to_table(res);
+            // Transform table into an iterator:
+            let mut iterator = table.into_iter().fuse();
+            // Closure that is called when this source node gets control:
+            move |output| {
+                if let Some(element) = iterator.next() {
+                    let mut session = output.session(capability.as_ref().unwrap());
+                    session.give(element);
+                } else {
+                    capability = None;
+                }
+            }
+        });
 
-        // let stream_1 = source(scope, "tables", move |capability| {
-        //     let mut table: Vec<Vec<String>> = Vec::new();
-        //
-        //     let mut iterator = table.into_iter().fuse();
-        //     let mut capability = Some(capability);
-        //
-        //     move |output| {
-        //         if let Some(element) = iterator.next() {
-        //             let mut session = output.session(capability.as_ref().unwrap());
-        //
-        //             session.give(element);
-        //         } else {
-        //             capability = None;
-        //         }
-        //     }
-        // });
-
-        // let stream_2 = source(scope, "tables", move |capability| {
-        //     let mut table: Vec<Vec<String>> = Vec::new();
-        //
-        //     let mut iterator = table.into_iter().fuse();
-        //     let mut capability = Some(capability);
-        //
-        //     move |output| {
-        //         if let Some(element) = iterator.next() {
-        //             let mut session = output.session(capability.as_ref().unwrap());
-        //
-        //             session.give(element);
-        //         } else {
-        //             capability = None;
-        //         }
-        //     }
-        // });
-
-        let mut input_table_1: Vec<Vec<String>> = Vec::new();
-        input_table_1.push(map2str(vec!["Ab1","100","200","Hi1"]));
-        input_table_1.push(map2str(vec!["Ab2","100","200","Hi2"]));
-        input_table_1.push(map2str(vec!["Ab3","200","500","Hi3"]));
-        input_table_1.push(map2str(vec!["Ab4","200","800","Hi4"]));
-        let stream_1 = input_table_1.to_stream(scope);
-
-        let mut input_table_2: Vec<Vec<String>> = Vec::new();
-        input_table_2.push(map2str(vec!["200","800","Bye1"]));
-        input_table_2.push(map2str(vec!["100","200","Bye2"]));
-        input_table_2.push(map2str(vec!["100","400","Bye3"]));
-        input_table_2.push(map2str(vec!["Ab3","200","500","Hi3"]));
-        let stream_2 = input_table_2.to_stream(scope);
-
-        stream_1.hash_join(&stream_2,vec![1,2],vec![0,1]).inspect(|x| println!("{:?}",x));
+        // Perform a hash_join by the name columns, then inspect to print the resulting entries:
+        stream_1.hash_join(&stream_2,vec![1],vec![1]).inspect(|x| println!("{:?}",x));
 
     });
 }
